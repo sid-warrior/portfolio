823c8afae4ba8affc84c1ef1018c0c5a251e3ede
Sidhant <sidhant2425@gmail.com>
2025-09-13 12:15:05 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-import UmamiAnalytics from '@/components/analytics/UmamiAnalytics';
 import Navbar from '@/components/common/Navbar';
 import OnekoCat from '@/components/common/OnekoCat';
 import Providers from '@/components/common/Providers';
-import { Quote } from '@/components/common/Quote';
 import { generateMetadata as getMetadata } from '@/config/Meta';
 import ReactLenis from 'lenis/react';
 import { ViewTransitions } from 'next-view-transitions';
@@ -18,15 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <ViewTransitions>
-      <html lang="en">
+      <html lang="">
         <body className={`font-hanken-grotesk antialiased`}>
           <Providers>
             <ReactLenis root>
               <Navbar />
               {children}
               <OnekoCat />
-              <Quote />
-              <UmamiAnalytics />
             </ReactLenis>
           </Providers>
         </body>
